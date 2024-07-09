6c747f82035e92cdbb62e66e7edd2637e23c7ed9
Lungeli <kawai.magar7@gmail.com>
2024-07-10 00:47:26 +0545

    Setting up,html and css structure

--- src/App.js
+++ src/App.js
@@ -3,22 +3,62 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <main>
+    <h1>Rs 4000<span>.00</span></h1>
+    <form>
+      <div className='basic'>
+         <input type="text" placeholder={'+500 new mousepad'}/>
+         <input type="datetime-local"/>
+      </div>
+      <div className='description'>
+        <input type="text" placeholder={'description of transction'}/>
+      </div>
+      <button type='submit'>Add New Transaction</button>
+    </form>
+    <div className='transactions'>
+     
+     
+     
+      <div className='transaction'>
+        <div className='left'>
+          <div className='name'>New Mosue Pad</div>
+          <div className='description'> Need to replace old mat</div>
+          </div>
+        <div className='right'>
+          <div className='price red'>- Rs 500</div>
+          <div className='datetime'>2024-04-12 15:45</div>
+
+        </div>
+      </div>
+
+      <div className='transaction'>
+        <div className='left'>
+          <div className='name'>Gig Income</div>
+          <div className='description'> Personal gig income</div>
+          </div>
+        <div className='right'>
+          <div className='price green'>+ Rs 1000</div>
+          <div className='datetime'>2024-04-12 15:45</div>
+
+        </div>
+      </div>
+
+
+      <div className='transaction'>
+        <div className='left'>
+          <div className='name'>Keyboard</div>
+          <div className='description'> Old keyboard was not working</div>
+          </div>
+        <div className='right'>
+          <div className='price red'>- Rs 1000</div>
+          <div className='datetime'>2024-04-12 15:45</div>
+
+        </div>
+      </div>
+
+    
+      </div>
+  </main>
   );
 }
 
